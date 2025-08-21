500e1e0c7095ef891a525c563946293f69e8febf
unknown4ser <153002928+unknown4ser@users.noreply.github.com>
2025-08-21 19:14:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//Navigation sidebar
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+//Button View Stall
+function showBio(){
+    window.location.href = "biodata.html";
+}
+
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+});
